b3e872a40508e5c6052ac426b5fff7903263f3bb
PaulineChane <pauline.chane@gmail.com>
2021-03-19 02:07:06 -0700

    newman conway tests passing, need to add time complexity

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -4,5 +4,20 @@
 def max_sub_array(nums)
     return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+    cur_max = next_max = 0
+
+    nums.each do |i|
+        next_max += nums
+        if (next_max < 0)
+            next_max = 0
+        elsif (cur_max < next_max)
+            cur_mar = next_max
+        end
+    end
+
+    return next_max
+end
+
+def max_sub_array_helper(nums)
+    
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,27 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError, "Out of range" if num < 1
+  nc_arr = [0, 1, 1] # zero index helps with indexing
+
+  return_array = nc_helper(nc_arr, num)
+
+  return return_array[1..num].join(' ') # this is an O(n) operation that doesn't nest in nc_helper
 end
+
+def nc_helper(nc_arr, num)
+  cur = nc_arr.length # i finally checked and this is indeed O(1) in ruby
+  
+  if (num > 2 && cur <= num)
+
+    result = nc_arr[nc_arr[cur - 1]] + nc_arr[(cur - nc_arr[cur - 1])]
+
+    nc_arr << result
+
+    nc_helper(nc_arr, num) 
+  end
+
+  return nc_arr
+end
+
+puts newman_conway(1)
