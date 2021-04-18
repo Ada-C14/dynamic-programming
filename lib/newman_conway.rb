37339f0c1185519c2b7f4a776297564c30b542d6
Anna Kim <anna.kim93@gmail.com>
2021-04-18 16:26:35 -0700

    complete newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,37 @@
-
-
 # Time complexity: ?
 # Space Complexity: ?
+#
+# PSEUDOCODE:
+# >> memoize previous items in sequence
+
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError, 'Input must be greater than 0' if num < 1
+  return '1' if num == 1
+  return '1 1' if num == 2
+
+  nc_store = { 1 => 1, 2 => 1 }
+
+  create_store(nc_store, num)
+
+  output_str = ''
+  counter = 0
+  nc_store.each do |k, v|
+    if counter == nc_store.size - 1
+      output_str << v.to_s
+    else
+      output_str << v.to_s + ' '
+    end
+    counter += 1
+  end
+
+  return output_str
+end
+
+def create_store(hash, num)
+  prev_item = 3
+  until prev_item > num
+    # P(P(n - 1)) + P(n - P(n - 1))
+    hash[prev_item] = hash[hash[prev_item - 1]] + hash[prev_item - hash[prev_item - 1]]
+    prev_item += 1
+  end
 end
