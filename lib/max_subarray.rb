f4b4819d9b062a852ebaaccf9f5b16bbe9a20464
Anna Kim <anna.kim93@gmail.com>
2021-04-18 17:23:05 -0700

    max_subarray method

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,29 @@
+# Time Complexity: O(n)
+# >> iterate through all nums in input arr
+#
+# Space Complexity: O(1)
+# >> constant to keep track of current_max and overall_max integers
+#
+# PSEUDOCODE:
+# >> for each iteration:
+# >> add to the current_max if the sum is greater than the number itself
+# >> otherwise, update current_max to the current num
+# >> check if we need to update the overall_max after updating the current_max
 
-# Time Complexity: ?
-# Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return 0 if nums.nil?
+
+  overall_max = current_max = nil
+  nums.each do |num|
+    if current_max.nil?
+      current_max = overall_max = num
+    elsif num + current_max > num
+      current_max += num
+    else
+      current_max = num
+    end
+    overall_max = current_max if current_max && current_max > overall_max
+  end
+
+  return overall_max
 end
