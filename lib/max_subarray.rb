2967f1b0cefc354981e956f2110c82ea64c4a6d4
Tram Bui <pbui17@cmc.edu>
2021-05-02 11:06:05 -0500

    max_sub_array

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,19 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) - iterate through the whole array
+# Space Complexity: O(1) - constant variables
 def max_sub_array(nums)
     return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+    # there can be all negative numbers in the nums array
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    i = 1 
+    while i < nums.length
+        max_ending_here = [nums[i], max_ending_here + nums[i]].max
+        max_so_far = [max_so_far, max_ending_here].max
+        i += 1
+    end
+
+    max_so_far
 end
