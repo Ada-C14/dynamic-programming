5ea0d393370ca2d1e563e610c1201e20c4fb87d4
Tram Bui <pbui17@cmc.edu>
2021-05-02 11:17:12 -0500

    refactor max_sub_array

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,13 +2,13 @@
 # Time Complexity: O(n) - iterate through the whole array
 # Space Complexity: O(1) - constant variables
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.empty?
     
     # there can be all negative numbers in the nums array
-    max_so_far = nums[0]
-    max_ending_here = nums[0]
+    max_so_far = nums.first
+    max_ending_here = nums.first
 
-    1.upto(nums.length-1) do |i|
+    1.upto(nums.length - 1) do |i|
         max_ending_here = [nums[i], max_ending_here + nums[i]].max
         max_so_far = [max_so_far, max_ending_here].max
     end
