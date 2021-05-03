3cdac932c12dcdbaa582ff1b03e7ebc4db1d89ff
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-05-02 21:54:08 -0700

    add newman conway logic

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,13 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  sequence = [0, 1, 1]
+
+  raise ArgumentError if num == 0
+
+  (3..num).each do |i|
+    sequence << sequence[sequence[i - 1]] + sequence[i - (sequence[i - 1])]
+  end
+
+  return sequence[1..num].join(" ")
 end
