c4f8d3c3319d67fb555627cc6fe86fa81eabc08d
Christabel Sebastian <christabel.escarez@gmail.com>
2021-05-03 21:09:49 -0700

    implemented max_sub_array

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -9,12 +9,14 @@ def newman_conway(num)
   raise ArgumentError if num.nil? || num < 1
 
   values = [0, 1, 1]
-  output_string = ""
+  output_string = "1"
+
   3.upto(num) do |i|
     values[i] = values[values[i-1]] + values[i - values[i-1]]
   end
-  1.upto(num) do |i|
-    output_string << "#{values[i]} "
+  2.upto(num) do |i|
+    output_string << " #{values[i]}"
   end
-  return output_string[0..-2]
+
+  return output_string
 end
