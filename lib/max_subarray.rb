b483181df807d7b1de9a01ca4f0ca63a52f8a996
Ren Carothers <rencarothers@gmail.com>
2021-05-03 19:49:17 -0700

    first pass

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,39 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nil if nums.empty?
+
+    # Initialize:
+    max_so_far = max_ending_here = nums[0]
+
+    # Loop for each element of the array
+    #          (a) max_ending_here = max_ending_here + a[i]
+    #          (b) if(max_ending_here < 0)
+    #          max_ending_here = 0
+    #          (c) if(max_so_far < max_ending_here)
+    #          max_so_far = max_ending_here
+    #          return max_so_far
+
+    (1...nums.length).each do |i|
+        max_ending_here = [nums[i], (max_ending_here + nums[i])].max
+        max_so_far = [max_so_far, max_ending_here].max
+    end
+
+    return max_so_far
 end
+
+# test output was hard to read
+# input = [-2,1,-3,4,-1,2,1,-5,4]
+# pp max_sub_array(input)
+# input = [-3, -4, -5, -6, -7]
+# pp max_sub_array(input)
+# input = [ -4, -5, -6, -7, -3]
+# pp max_sub_array(input)
+# input = [3]
+# pp max_sub_array(input)
+# input = []
+# pp max_sub_array(input)
+# input = [50, -50, 50]
+# pp max_sub_array(input)
