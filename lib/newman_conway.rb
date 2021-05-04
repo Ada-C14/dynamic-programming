f664a98d384b3bb8ad802ec49710e346648e8ce4
Christabel Sebastian <christabel.escarez@gmail.com>
2021-05-03 20:07:32 -0700

    implemented newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,20 @@
+# P(1) = 1
+# P(2) = 1
+# for all n > 2
+# P(n) = P(P(n - 1)) + P(n - P(n - 1))
 
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num.nil? || num < 1
+
+  values = [0, 1, 1]
+  output_string = ""
+  3.upto(num) do |i|
+    values[i] = values[values[i-1]] + values[i - values[i-1]]
+  end
+  1.upto(num) do |i|
+    output_string << "#{values[i]} "
+  end
+  return output_string[0..-2]
 end
