a497d128c43cee3f7fc64709d9b740735b45d448
beauttie <15842537+beauttie@users.noreply.github.com>
2021-05-05 00:07:59 -0700

    Implement newman_conway to pass tests

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,26 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+
+  solutions = [1, 1]
+  if num == 1
+    return solutions.first.to_s
+  elsif num == 2
+    return "#{solutions[0]} #{solutions[1]}"
+  end
+
+  current = 3
+  while current <= num
+    # indices start at 0, not at 1 => subtract one from each index reference
+    solutions << solutions[solutions[current - 2] - 1] + solutions[current - solutions[current - 2] - 1]
+    current += 1
+  end
+
+  sequence = ""
+  (solutions.length - 1).times do |i|
+    sequence << "#{solutions[i]} "
+  end
+
+  return sequence << "#{solutions.last.to_s}"
 end
