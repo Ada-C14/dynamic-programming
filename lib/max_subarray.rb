1964dfc92fa3069729760c08a1d9ea9e4b4477cd
Alice Boone <alicegbone@gmail.com>
2021-05-06 17:00:31 -0700

    max_subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -4,5 +4,14 @@
 def max_sub_array(nums)
     return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max = nums[0]
+    max_sum = nums[0]
+    i = 1
+
+    while i <= (nums.length - 1)
+        max = [nums[i], (max + nums[i])].max
+        max_sum = max if max > max_sum
+        i += 1
+    end
+    return max_sum
 end
