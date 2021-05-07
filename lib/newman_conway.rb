77bba6bb5ae2f5c1a9b334ab42591259235862d9
beauttie <15842537+beauttie@users.noreply.github.com>
2021-05-07 01:31:37 -0700

    DRY up newman_conway method

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,28 +1,18 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n) because of array
 def newman_conway(num)
   raise ArgumentError if num < 1
 
-  solutions = [1, 1]
-  if num == 1
-    return solutions.first.to_s
-  elsif num == 2
-    return "#{solutions[0]} #{solutions[1]}"
-  end
+  return "1" if num == 1
 
+  solutions = [nil, 1, 1]
   current = 3
   while current <= num
-    # indices start at 0, not at 1 => subtract one from each index reference
-    solutions << solutions[solutions[current - 2] - 1] + solutions[current - solutions[current - 2] - 1]
+    solutions << solutions[solutions[current - 1]] + solutions[current - solutions[current - 1]]
     current += 1
   end
 
-  sequence = ""
-  (solutions.length - 1).times do |i|
-    sequence << "#{solutions[i]} "
-  end
-
-  return sequence << "#{solutions.last.to_s}"
+  return solutions[1..-1].join(" ")
 end
