821c31b8bd42384d37a7d6b771ff1155efbe0f67
Alice Boone <alicegbone@gmail.com>
2021-05-06 17:01:02 -0700

    newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,23 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  array = Array.new(num, 0)
+  i = 1
+
+  while i <= num 
+    if i == 1 || i == 2
+      array[i] = 1
+    else
+      array[i] = array[array[i - 1]] + array[i - array[i-1]]
+    end
+    i += 1
+  end
+
+  array.shift # remove first element
+  
+  return array.join(' ')
 end
