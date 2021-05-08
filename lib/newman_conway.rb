9168ba716693130b0de88e415945243e1f2f6ac3
MonaRahmani <monarahmanimehr@gmail.com>
2021-05-08 14:38:17 -0700

    newman_conway done

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,18 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  result = [0, 1, 1]
+  i = 3
+
+  while i <= num
+    result << result[result[i - 1]] + result[i - result[i - 1]] #P(n) = P(P(n - 1)) + P(n - P(n - 1))
+    i += 1
+  end
+  p result
+
+  result[1..-1].join(' ') # remove 0 
 end
