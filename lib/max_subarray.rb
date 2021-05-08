caf7a7523beb18b63ca6cc77a91f83cd2bccaf3b
roshni-patel <rpatel01@wesleyan.edu>
2021-05-08 00:14:50 -0500

    Completed max subarray problem with time and space complexities

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,33 @@
+# Kadane's algorithm
+# Initialize:
+#     max_so_far = 0
+#     max_ending_here = 0
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Loop for each element of the array
+#   (a) max_ending_here = max_ending_here + a[i]
+#   (b) if(max_ending_here < 0)
+#             max_ending_here = 0
+#   (c) if(max_so_far < max_ending_here)
+#             max_so_far = max_ending_here
+# return max_so_far
+
+
+# Time Complexity: O(n) to iterate through the entire nums array 
+# Space Complexity: O(1) given that the space required for max_so_far and max_ending_here do not change with the size of nums
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.empty? 
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max_so_far = nums[0]
+    max_ending_here = 0
+
+    nums.each do |num|
+        max_ending_here = max_ending_here + num 
+        if max_so_far < max_ending_here
+            max_so_far = max_ending_here
+        elsif
+            max_ending_here < 0
+            max_ending_here = 0
+        end
+    end
+    return max_so_far
 end
