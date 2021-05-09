aa13d373043f99589dd5abf6cb2cebaecfdf445d
Kareha Agesa <kmaagesa@gmail.com>
2021-05-08 18:02:25 -0700

    first maxsubarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,33 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: o(n)
+# Space Complexity: o(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return nil if nums.empty?
+
+  current_max = nums[0]
+  max = nums[0]
+
+  nums.each_with_index do |num, index|
+    if index != 0
+      current_max = [current_max + num, num].max
+      max = [max, current_max].max
+    end
+  end
+  return max
+  
+  # o(n^2) way
+  # max = nums[0]
+  # nums.each_with_index do |num_1, index_1|
+  #   current_sum = num_1
+  #   j = index_1
+  #   j.upto(nums.length - 1) do |index_2|
+  #     if index_1 == index_2
+  #       max = nums[index_1] > max ? nums[index_1] : max
+  #     else
+  #       current_sum += nums[index_2]
+  #       max = current_sum > max ? current_sum : max
+  #     end
+  #   end
+  # end
+  # return max
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,14 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError.new("cant be less than 1") if num < 1
+
+  return "1" if num == 1
+  array = [nil, 1, 1]
+
+  3.upto(num) do |i|                                              # time: o(n)
+    array[i] = array[array[i - 1]] + array[i - array[i - 1]]
+  end
+
+  return array[1..-1].join(" ")                                   # space: o(n), time: o(n)
 end
