62a6d6833473e3789f08a97904a3cf330b0ecb31
Kareha Agesa <kmaagesa@gmail.com>
2021-05-08 18:44:42 -0700

    2 ways for max subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -4,15 +4,31 @@
 def max_sub_array(nums)
   return nil if nums.empty?
 
-  current_max = nums[0]
-  max = nums[0]
+  # current_max = nums[0]
+  # max = nums[0]
 
-  nums.each_with_index do |num, index|
-    if index != 0
-      current_max = [current_max + num, num].max
-      max = [max, current_max].max
+  # nums.each_with_index do |num, index|
+  #   if index != 0
+  #     current_max = [current_max + num, num].max
+  #     max = [max, current_max].max
+  #   end
+  # end
+  # return max
+
+  return nil if nums.empty?
+
+  max = nums[0]
+  current_max = 0
+  nums.each do |num|
+    current_max += num
+    if max < current_max
+        max = current_max
+    end
+    if current_max < 0
+      current_max = 0
     end
   end
+
   return max
-  
+
 end
