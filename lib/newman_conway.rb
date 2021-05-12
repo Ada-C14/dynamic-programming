1839cc6af85a913c8f17faa9da751f1c936115b7
Yinjing Lin <lyingjin@amazon.com>
2021-05-11 22:13:02 -0700

    complete newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,15 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  p = [0, 1, 1]
+  
+  i = 3
+  while i <= num
+    p << p[p[i - 1]] + p[i - p[i - 1]]
+    
+    i += 1
+  end
+
+  return p[1..num].join(" ")
 end
