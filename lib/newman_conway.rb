e2b6e3197bca14d7a1633049e7ae82e4eee5d3f3
OlgaSe <olga.tka4eva@gmail.com>
2021-05-13 16:08:27 -0700

    completed newman_conway task

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,25 @@
 
-
-# Time complexity: ?
-# Space Complexity: ?
+# P(n) = P(P(n - 1)) + P(n - P(n - 1))
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+
+  result = [0, 1, 1]
+
+  i = 3
+  while i <= num
+    result[i] = result[result[i - 1]] + result[i - result[i - 1]]
+    i += 1
+  end
+
+  string = result[1..-1].join(" ")
+
+  return string
 end
+
+# num = 5
+# i: 3
+# result[i]: 3
+# result: 0, 1, 1, 
