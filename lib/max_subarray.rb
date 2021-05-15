3bab2ae1fdb21a4b91cf2233b378969ef97ca57b
Mackenzie <mvlofthus@gmail.com>
2021-05-14 20:38:50 -0700

    max_subarray implemented

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,6 +3,30 @@
 # Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nil if nums == []
+    return nums[0] if nums.length == 1 
+
+    max_so_far = nums[0]
+    max_ending_here = 0
+
+    nums.each_with_index do |num, index| 
+        i = index + 1 
+
+        if num > max_so_far
+            max_so_far = num
+        end
+
+        max_ending_here = num 
+
+        while  i < nums.length do 
+            max_ending_here = max_ending_here + nums[i]
+                if (max_so_far < max_ending_here)
+                    max_so_far = max_ending_here
+                end
+            i+=1
+        end
+    end
+
+
+    return max_so_far
 end
