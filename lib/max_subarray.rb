4bf0b64c70400e56ad4950a320106754da2a9dd1
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-05-21 19:26:16 -0700

    finalized wave 2

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,18 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.nil?
+
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    nums.each_with_index do |number, index|
+        if index > 0
+            max_ending_here = [number, max_ending_here + number].max
+            max_so_far = [max_so_far, max_ending_here].max
+        end
+    end
+
+    return max_so_far
     
-    raise NotImplementedError, "Method not implemented yet!"
 end
