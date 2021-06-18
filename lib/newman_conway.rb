7ffae4cb6b3942550e2adad8db9d06683f979980
Sophie <sophie.messing@realself.com>
2021-06-17 19:18:42 -0700

    newman-conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  return raise ArgumentError if num <= 0
+
+  array = [1]
+  index = 2
+
+  if num > 1
+    array << 1
+    while index < num
+      array << array[array[index - 1] - 1] + array[index - array[index - 1]]
+      index += 1
+    end
+  end
+
+  return array.join(' ')
+
 end
