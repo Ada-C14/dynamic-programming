5f2c36003b2ecc27fe8be96cf7e81efa321062f2
KayKay-git <kayalew@yahoo.com>
2021-06-26 20:20:37 -0400

    Methods complete, pass

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,16 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    # raise NotImplementedError, "Method not implemented yet!"
+    largest_sum = nums[0]
+    current = 0
+
+    nums.each do |n|
+        largest_sum = [n, largest_sum + n].max
+        current = [current, largest_sum].max
+    end 
+    return largest_sum
 end
