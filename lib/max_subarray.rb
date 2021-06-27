85dbdcc2da03edbfce222712516613fd91c708ff
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2021-06-26 19:02:42 -0700

    implement max sub array, tests passing

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,15 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    max_ending_here = nums[0]
+    max_so_far = nums[0]
+
+    for i in (1..nums.length - 1 ) do 
+        max_ending_here = [nums[i], max_ending_here + nums[i]].max
+        max_so_far = [max_so_far, max_ending_here].max 
+    end 
+    return max_so_far
+
 end
