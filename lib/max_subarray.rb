3f9ade590918c1bcdf9e19ccf34075a4f30240ad
Taylor Tompkins <taylormililani@gmail.com>
2021-07-12 15:04:53 -0700

    all tests passing

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,15 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    local_max = 0
+    max = nil
+    i = 0
+    until i == nums.length
+        local_max = [nums[i], nums[i] + local_max].max
+        if max == nil || max < local_max
+            max = local_max
+        end
+        i += 1
+    end
+    return max
 end
