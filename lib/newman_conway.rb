8b59dab3d53b895df0f1f5f00e9c43c985ed2ea3
Rachael Gomez <gomezrc1220@gmail.com>
2021-07-24 10:24:30 -0700

    all tests passing

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,26 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  if num == 0
+    raise ArgumentError
+  end
+  if num == 1
+    return "1"
+  end
+  
+  array = []
+  array[0] = 0
+  array[1] = 1
+  array[2] = 1
+
+  i = 3
+  
+  while i <= num
+    array[i] = array[array[i-1]] + array[i - array[i - 1]]
+    i += 1
+  end
+  
+  array.slice!(0)
+  
+  return array.join(',').gsub(',', ' ')
 end
