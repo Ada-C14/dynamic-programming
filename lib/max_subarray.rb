c81eab1cd62b3092dc5bb624b4dc4a06d7f6511a
spv-hub <sandyvasquez9@gmail.com>
2021-07-25 14:24:48 -0700

    First problem, done

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,18 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    max_amount = nums[0]
+    current_amount = 0
+    nums.each do |num|
+        current_amount += num
+        if max_amount < current_amount
+            max_amount = current_amount
+        elsif  current_amount < 0
+            current_amount = 0
+        end
+    end
+    return max_amount
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,4 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
 end
