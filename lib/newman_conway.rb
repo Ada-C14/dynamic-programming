0e32e1140a6812ae3a18f4f0e96099f79279fc31
spv-hub <sandyvasquez9@gmail.com>
2021-07-25 14:41:09 -0700

    Second problem, done

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,6 +1,18 @@
-
-
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
+raise ArgumentError if num <= 0
+
+return '1' if num == 1
+return '1 1' if num == 2
+
+record = [0, 1, 1]
+i = 3
+while (i <= num) 
+    record[i] = record[record[i - 1]] + record[i - record[i - 1]]
+    i += 1
+  end
+  record[1..-1].join(' ')
 end
+
+
