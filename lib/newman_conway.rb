5163ab1d7bae1f79620a119fbddb00d548ee479d
Hanh Solo <minhhanhnyc@gmail.com>
2021-07-25 18:55:46 -0700

    submissions

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,16 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) where n is num 
+# Space Complexity: O(n) function stack call & array of (n) values
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  return newman_helper(num, 3, [0, 1, 1]).slice(1, num).join(" ")
+end
+
+def newman_helper(num, current, memo)
+  raise ArgumentError if num == 0
+  if current > 2 && current <= num
+    current_idx = current 
+    memo[current_idx] = memo[memo[current_idx - 1]] + memo[current_idx - memo[current_idx - 1]]
+    newman_helper(num, current + 1, memo)
+  end
+
+  return memo
 end
